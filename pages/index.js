c0e501784923094e2a6d5f53381189dec8f36edd
Sigrun <sigrunjj.eis@gmail.com>
2021-01-31 18:47:22 +0100

    Grid commit - created table - found svg for squares

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Tile from '../components/tile'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -14,41 +13,26 @@ export default function Home() {
           Welcome to Siggi's Blog
         </h1>
 
-        <Tile text='hello' color='green' />
-
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          My evolving portfolio, created by{' '}
+          <code className={styles.code}>learning</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <table class={styles.table}>
+            <tbody>
+              <tr>
+                <td><img src="/oneSquare.svg" class={styles.square} /></td>
+                <td><img src="/oneSquare.svg" class={styles.square} /></td>
+                <td><img src="/oneSquare.svg" class={styles.square} /></td>
+              </tr>
+              <tr>
+                <td><img src="/oneSquare.svg" class={styles.square} /></td>
+                <td><img src="/oneSquare.svg" class={styles.square} /></td>
+                <td><img src="/oneSquare.svg" class={styles.square} /></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </main>
 
